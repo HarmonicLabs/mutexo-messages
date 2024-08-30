6fbb3c696fb0213e282e9d4649afaf2cbcb37c4f
Raffa Bux <raffaelebussolotto@gmail.com>
2024-08-30 16:08:33 +0200

    utils update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,2 @@
-export const maxBufferSize: number = 100;
-export const minCode: number = 0;
-export const maxCode: number = 7;
+export const minTypeCode = 0;
+export const maxTypeCode = 7;
--- src/utils/isThatType.ts
+++ src/utils/isThatType.ts
@@ -1,11 +1,69 @@
-export function isByte( stuff: any ): boolean {
-    if( typeof stuff === "bigint" ) {
-        return( stuff >= 0 && stuff <= 255 );
-    }
-    
-    return( Number.isSafeInteger( stuff ) && ( stuff >= 0 && stuff <= 255 ) );
+const hexChars = Object.freeze( Array.from( "0123456789abcdef" ) );
+
+export function isBoolean( stuff: any ): boolean 
+{
+    return( typeof stuff === "boolean" );
 }
 
-export function areBytes( stuff: Uint8Array ) {
+export function isHash( stuff: any ): boolean 
+{
     return( stuff instanceof Uint8Array );
+}
+
+export function isHash32( stuff: any ): boolean 
+{
+    return( 
+        isHash( stuff ) &&
+        stuff.length === 32
+    );
+}
+
+export function isHash28( stuff: any ): boolean 
+{
+    return( 
+        isHash( stuff ) &&
+        stuff.length === 28
+    );
+}
+
+export function isHex( stuff: string ): boolean 
+{
+    if(!( typeof stuff === "string" ))  return false;
+
+    return Array.from( stuff ).every( ( char ) => hexChars.includes( char ) );
+}
+
+function isThatSize( stuff: any, pow: number ): boolean 
+{
+    if( typeof stuff === "bigint" ) {
+        return( 
+            stuff >= 0 && 
+            stuff < Math.pow( 2, pow ) 
+        );
+    }
+    
+    return( 
+        Number.isSafeInteger( stuff ) && 
+        ( stuff >= 0 && stuff < Math.pow( 2, pow ) ) 
+    );
+}
+
+export function isByte( stuff: any ): boolean 
+{
+    return isThatSize( stuff, 8 );
+}
+
+export function isWord16( stuff: any ): boolean
+{
+    return isThatSize( stuff, 16 );
+}
+
+export function isWord32( stuff: any ): boolean 
+{
+    return isThatSize( stuff, 32 );
+}
+
+export function isWord64( stuff: any ): boolean 
+{
+    return isThatSize( stuff, 64 );
 }
