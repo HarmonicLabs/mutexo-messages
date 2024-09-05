4e5a016e2d1eb463b586a466e1392604925a6078
Raffa Bux <raffaelebussolotto@gmail.com>
2024-09-05 17:24:21 +0200

    parsers update

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,4 +1,5 @@
 import { MutexoMessage, mutexoMessageFromCborObj } from "../MutexoMessage";
+import { ClientReq, clientReqFromCborObj } from "../ClientReq";
 import { isObject } from "@harmoniclabs/obj-utils";
 import { CborArray } from "@harmoniclabs/cbor";
 
@@ -10,4 +11,14 @@ export function parseMutexoMessage( stuff: any ): MutexoMessage
     )) throw new Error( "Invalid message" );
 
     return mutexoMessageFromCborObj( stuff );
+}
+
+export function parseClientReq( stuff: any ): ClientReq
+{
+    if(!( 
+        isObject( stuff ) &&
+        stuff instanceof CborArray 
+    )) throw new Error( "Invalid message" );
+
+    return clientReqFromCborObj( stuff );
 }
